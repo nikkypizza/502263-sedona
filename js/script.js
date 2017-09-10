add1da234d22ee17833842f2a0408da84d71383e
Nikita <30810655+nikkypizza@users.noreply.github.com>
2017-09-10 13:11:49 +0300

    Финальные правки

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 var link = document.querySelector(".find-hotel-button");
 var popup = document.querySelector(".booking");
-var close = popup.querySelector(".find-hotel-button");
 
 
-link.addEventListener("click", function (evt) {
+link = link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("booking-show");
-});
-
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("booking-show");
 });
